c9e24732ddf050b2ab6e22d5a1012670be6f8841
solo <solomonarnett@gmail.com>
2017-03-27 21:28:04 -0400

    modifed sponsors image

--- index.php
+++ index.php
@@ -99,7 +99,7 @@
 						<a class="modified-link sponsors" href="#sponsors"><span class="link">Sponsors</span></a>
 					</section>
 				</section>
-				<a class="register" href="http://my.mlh.io/oauth/authorize?client_id=83b1262b912a0d252d2d5f44c9506e8beaba4a67035ebe8d498f8dacc685d284&redirect_uri=http%3A%2F%2Fwww.hackgsu.com%2Fskills.php&response_type=token">Register</a>
+				<!-- <a class="register" href="http://my.mlh.io/oauth/authorize?client_id=83b1262b912a0d252d2d5f44c9506e8beaba4a67035ebe8d498f8dacc685d284&redirect_uri=http%3A%2F%2Fwww.hackgsu.com%2Fskills.php&response_type=token">Register</a> -->
 			</section>
 			<section class="schedule" id="schedule">
 				
